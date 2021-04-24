89ab9910f06f3236b3363b05d8fb86f67fe8c6ac
Joana Brito <joanafiorentini@hotmail.com>
2021-04-23 19:52:40 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,17 @@
+//Part II : MOD 0 Assessment
+
+var famousPerson = "Michael Jordan";
+var isActive = true;
+
+famousPerson = "Whitney Houston";
+
+var num3 = 3;
+var num9 = 9;
+var sum = num9 + num3;
+console.log(sum);
+
+var animals = ["Dog", "Bird", "Cat"];
+animals.shift();
+console.log(animals);
+
+console.log(famousPerson.length);
