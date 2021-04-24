c0d31d44d582d60cea72558c2674ada1530fc8a6
Joana Brito <joanafiorentini@hotmail.com>
2021-04-23 20:15:26 -0700

    Add file modelA.js and work

---
+++ models/modelA.js
@@ -0,0 +1,15 @@
+//Part III : MOD 0 Assessment
+
+var airport = {
+  name: 'LAX',
+  city: 'Los Angeles',
+  opened: 1930,
+  numberOfTerminals: 9,
+  gym: false,
+  coffeShop: true,
+  storeTypes:['VIP rooms', 'Restaurants', 'Clothes', 'Gift shop'],
+  airlines:['American Airlines','Alaska Airlines','Avianca','Delta Air Lines','Japan Airlines'],
+}
+
+
+ console.log(airport);
