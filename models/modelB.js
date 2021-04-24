c9697e22eb22c72888a837793b4fe2651864e88c
Joana Brito <joanafiorentini@hotmail.com>
2021-04-23 20:29:50 -0700

    Add file modelB.js and work

---
+++ models/modelB.js
@@ -0,0 +1,16 @@
+//Part III : MOD 0 Assessment
+
+
+var vehicle = {
+  name: "BMW X1",
+  color: "red",
+  year: 2019,
+  price: 35000,
+  new: true,
+  used: false,
+  optionOfColors:['Black', 'White', 'Blue', 'Gray'],
+  exteriorFeatures:['Privacy glass','Satin Aluminum roof rails','Non-metallic paint'],
+
+}
+
+console.log(vehicle);
